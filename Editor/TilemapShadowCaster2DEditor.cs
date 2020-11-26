455a83041bfe1b5785d209208a31519c5259413b
Brian Craig <briancraigok@gmail.com>
2020-11-26 16:01:25 -0300

    Initial Version

---
+++ Editor/TilemapShadowCaster2DEditor.cs
@@ -0,0 +1,27 @@
+﻿using TilemapShadowCaster.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace TilemapShadowCaster.Editor
+{
+    [CustomEditor(typeof(TilemapShadowCaster2D))]
+    public class TilemapShadowCaster2DEditor : UnityEditor.Editor
+    {
+        private SerializedProperty m_selfShadowsProperty;
+
+        void OnEnable()
+        {
+            m_selfShadowsProperty = serializedObject.FindProperty("m_SelfShadows");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.PropertyField(m_selfShadowsProperty, new GUIContent("Self Shadows"));
+            if (serializedObject.hasModifiedProperties)
+            {
+                serializedObject.ApplyModifiedProperties();
+                ((MonoBehaviour) target).GetComponent<TilemapShadowCaster2D>().ReinitializeShapes();
+            }
+        }
+    }
+}
---
+++ Runtime/PathShadow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace TilemapShadowCaster.Runtime
+{
+    public class PathShadow : ShadowCaster2D
+    {
+        static FieldInfo shapeFieldInfo = typeof(ShadowCaster2D).GetField("m_ShapePath",
+            BindingFlags.NonPublic | BindingFlags.Instance);
+
+        static FieldInfo shapeHashFieldInfo = typeof(ShadowCaster2D).GetField("m_ShapePathHash",
+            BindingFlags.NonPublic | BindingFlags.Instance);
+
+        internal void SetShape(List<Vector2> points)
+        {
+            Vector3[] shapev3 = points.ConvertAll((point) => new Vector3(point.x, point.y)).ToArray();
+            shapeFieldInfo.SetValue(this, shapev3);
+            shapeHashFieldInfo.SetValue(this, (int) UnityEngine.Random.Range(0f, 10000000f));
+        }
+    }
+}
