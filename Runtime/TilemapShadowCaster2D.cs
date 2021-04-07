e053d074af871e923ec6cf8904a8a4f89b01c93e
Rees Varney <reesvarney02@gmail.com>
2021-04-07 11:33:44 +0100

    Remove old code - again

--- Runtime/TilemapShadowCaster2D.cs
+++ Runtime/TilemapShadowCaster2D.cs
@@ -85,12 +85,4 @@ namespace TilemapShadowCaster.Runtime
         }
     }
 
-     public class BitMaskAttribute : PropertyAttribute
-    {
-        public System.Type propType;
-        public BitMaskAttribute(System.Type aType)
-        {
-            propType = aType;
-        }
-    }
 }
